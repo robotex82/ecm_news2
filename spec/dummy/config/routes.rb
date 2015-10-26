532abbc6bffbaead3aa31f48ac1330b3c53a1fa4
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2015-10-26 04:26:24 +0100

    Enhancement: The back button in the news index now links to main_app.root_path.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   Ecm::News::Routing.routes(self)
+
+  root to: 'ecm/news/items#index'
 end
