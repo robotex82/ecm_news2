c71f53b681b25c74c899476894447ad8319c2908
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-02-26 10:51:59 +0100

    Fixed controller inheritance. Fixed pagination rendering.

--- app/controllers/ecm/news/items_controller.rb
+++ app/controllers/ecm/news/items_controller.rb
@@ -1,4 +1,4 @@
-class Ecm::News::ItemsController < Ecm::Pictures::Configuration.base_controller.constantize
+class Ecm::News::ItemsController < Ecm::News::Configuration.base_controller.constantize
   def index
     @items = Ecm::News::Item.published.where(locale: I18n.locale.to_s).order('published_at DESC').page(params[:page]).per(5)
   end
